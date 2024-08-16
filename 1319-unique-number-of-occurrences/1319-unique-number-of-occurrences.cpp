fce82efd62b96198ece0702aee16baa2629eb045
Amit Kumar <108882264+AmitJNV-IIIT@users.noreply.github.com>
2024-08-16 17:29:00 +0530

    Time: 0 ms (100%), Space: 11.6 MB (24.15%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map< int , int> mp; 
+        
+        for(int &x : arr){// store frequencies of numbers in map 
+            mp[x]++;
+        }
+        unordered_set<int>st;
+        for(auto &it : mp){
+            int freq = it.second; // it.second contains frequency & it.first contains number
+            if(st.find(freq) != st.end()){
+                // we have found duplicate frequency in set
+                return false;
+            }
+            st.insert(freq);// insert in set as previously it wasn't in set
+        }
+        return true;
+    }
+};
