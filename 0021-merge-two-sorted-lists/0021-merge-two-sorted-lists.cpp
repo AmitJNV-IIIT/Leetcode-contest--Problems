57a28cb6c566d0bc413035c2cbd0d442ebfca6f0
Amit Kumar <108882264+AmitJNV-IIIT@users.noreply.github.com>
2024-08-21 22:20:15 +0530

    Time: 8 ms (25.68%), Space: 19.7 MB (39.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -36,13 +36,15 @@ public:
         {
             if(list1 -> val < list2 -> val){
                 curr->next = list1;
+                curr=list1;
                 list1 = list1 -> next;
             }
             else{
                 curr->next = list2;
+                curr=list2;
                 list2 = list2 -> next;
             }
-            curr = curr -> next;
+            //curr = curr -> next;
                 
         }
 		
