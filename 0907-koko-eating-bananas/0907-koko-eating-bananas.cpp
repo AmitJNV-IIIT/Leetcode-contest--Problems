1a15e20eb62bbee043f66eb80681d2fc6c37330e
Amit Kumar <108882264+AmitJNV-IIIT@users.noreply.github.com>
2024-09-07 16:55:04 +0530

    Time: 23 ms (94.72%), Space: 21.7 MB (78.98%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -27,3 +27,15 @@ public:
         return true;
     }
 };
+// Check if Koko can eat all the bananas at the current speed (middle pointer) within h hours using the canEatAll method.
+// c. If Koko can eat all the bananas at the current speed, update the right pointer to the middle pointer using right = mid.
+// d. If Koko cannot eat all the bananas at the current speed, update the left pointer to mid + 1.
+// Once the left pointer is equal to the right pointer, return the left pointer as the minimum speed at which Koko can eat all the bananas within h hours.
+// The canEatAll method calculates the total time required to eat all the piles using the given speed. If the total time is greater than h, the method returns false, otherwise, it
+// returns true.
+// Complexity
+// Time complexity:
+// The binary search algorithm has a time complexity of O(logn), where n is the maximum number of bananas in a pile. The canEatAll function has a time complexity of O(n), where n is the number of piles. Therefore, the overall time complexity of the solution is O(nlogn).
+
+// Space complexity:
+// The space complexity of the solution is O(1), as we only use a constant amount of extra space.
