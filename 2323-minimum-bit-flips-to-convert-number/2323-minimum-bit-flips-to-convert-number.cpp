cab5b9d38212a4cc7625d15f1402db0e5406c4f0
Amit Kumar <108882264+AmitJNV-IIIT@users.noreply.github.com>
2024-09-11 18:01:01 +0530

    Time: 0 ms (100%), Space: 7.7 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -3,9 +3,9 @@ public:
     int minBitFlips(int start, int goal) {
         int c=0;
         while(start!=0 || goal!=0){
-       if((start&1) !=(goal&1)){
+       if((start&1) !=(goal&1)){//find right most bit(MSD)
          c++;
-       } start>>=1;
+       } start>>=1;//right shift by 1 bu 1 is done.
        goal>>=1;
         }return c;
     }
